26eeb0b65df47b0c69254a87e50a72cf3e7279d6
Rosataker <pechin1219@gmail.com>
2018-05-10 13:56:50 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(137, 426);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,445 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        const int DRAG_HANDLE_SIZE = 7;
+        int mouseX, mouseY;
+        Control SelectedControl;
+        Direction direction;
+        Point newLocation;
+        Size newSize;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //start the timer that will be monitoring the mouse movement every 500 ms.
+            timer1.Start();
+        }
+
+
+
+        /// <summary>
+        /// Resize the selected control, minimum width and height is 20.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (SelectedControl != null && e.Button == MouseButtons.Left)
+            {
+                timer1.Stop();
+                Invalidate();
+
+                if (SelectedControl.Height < 20)
+                {
+                    SelectedControl.Height = 20;
+                    direction = Direction.None;
+                    Cursor = Cursors.Default;
+                    return;
+                }
+                else if (SelectedControl.Width < 20)
+                {
+                    SelectedControl.Width = 20;
+                    direction = Direction.None;
+                    Cursor = Cursors.Default;
+                    return;
+                }
+
+                //get the current mouse position relative the the app
+                Point pos = this.PointToClient(MousePosition);
+
+                #region resize the control in 8 directions
+                if (direction == Direction.NW)
+                {
+                    //north west, location and width, height change
+                    newLocation = new Point(pos.X, pos.Y);
+                    newSize = new Size(SelectedControl.Size.Width - (newLocation.X - SelectedControl.Location.X),
+                        SelectedControl.Size.Height - (newLocation.Y - SelectedControl.Location.Y));
+                    SelectedControl.Location = newLocation;
+                    SelectedControl.Size = newSize;
+                }
+                else if (direction == Direction.SE)
+                {
+                    //south east, width and height change
+                    newLocation = new Point(pos.X, pos.Y);
+                    newSize = new Size(SelectedControl.Size.Width + (newLocation.X - SelectedControl.Size.Width - SelectedControl.Location.X),
+                        SelectedControl.Height + (newLocation.Y - SelectedControl.Height - SelectedControl.Location.Y));
+                    SelectedControl.Size = newSize;
+                }
+                else if (direction == Direction.N)
+                {
+                    //north, location and height change
+                    newLocation = new Point(SelectedControl.Location.X, pos.Y);
+                    newSize = new Size(SelectedControl.Width,
+                        SelectedControl.Height - (pos.Y - SelectedControl.Location.Y));
+                    SelectedControl.Location = newLocation;
+                    SelectedControl.Size = newSize;
+                }
+                else if (direction == Direction.S)
+                {
+                    //south, only the height changes
+                    newLocation = new Point(pos.X, pos.Y);
+                    newSize = new Size(SelectedControl.Width,
+                        pos.Y - SelectedControl.Location.Y);
+                    SelectedControl.Size = newSize;
+                }
+                else if (direction == Direction.W)
+                {
+                    //west, location and width will change
+                    newLocation = new Point(pos.X, SelectedControl.Location.Y);
+                    newSize = new Size(SelectedControl.Width - (pos.X - SelectedControl.Location.X),
+                        SelectedControl.Height);
+                    SelectedControl.Location = newLocation;
+                    SelectedControl.Size = newSize;
+                }
+                else if (direction == Direction.E)
+                {
+                    //east, only width changes
+                    newLocation = new Point(pos.X, pos.Y);
+                    newSize = new Size(pos.X - SelectedControl.Location.X,
+                        SelectedControl.Height);
+                    SelectedControl.Size = newSize;
+                }
+                else if (direction == Direction.SW)
+                {
+                    //south west, location, width and height change
+                    newLocation = new Point(pos.X, SelectedControl.Location.Y);
+                    newSize = new Size(SelectedControl.Width - (pos.X - SelectedControl.Location.X),
+                        pos.Y - SelectedControl.Location.Y);
+                    SelectedControl.Location = newLocation;
+                    SelectedControl.Size = newSize;
+                }
+                else if (direction == Direction.NE)
+                {
+                    //north east, location, width and height change
+                    newLocation = new Point(SelectedControl.Location.X, pos.Y);
+                    newSize = new Size(pos.X - SelectedControl.Location.X,
+                        SelectedControl.Height - (pos.Y - SelectedControl.Location.Y));
+                    SelectedControl.Location = newLocation;
+                    SelectedControl.Size = newSize;
+                }
+                #endregion
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (SelectedControl != null)
+                DrawControlBorder(SelectedControl);
+            timer1.Start();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete && SelectedControl != null)
+            {
+                Controls.Remove(SelectedControl);
+                
+                Invalidate();
+            }
+        }
+
+        Control _ctrl = null;//存放被拖曳的控制項
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            //取出被拖曳的控制項
+            _ctrl = e.Data.GetData(e.Data.GetFormats(true)[0]) as Control;
+            if (_ctrl != null)
+                e.Effect = (_ctrl == null) ? DragDropEffects.None : DragDropEffects.Move;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (_ctrl != null)
+            {
+
+                string origin_str = _ctrl.GetType().ToString();
+                //MessageBox.Show(origin_str);
+                if (_ctrl is Button)
+                {
+
+                    if (_ctrl.Name == "button1")
+                    {
+                        Button newCtrl = new Button();
+                        newCtrl.Text = _ctrl.Text;
+                        newCtrl.MouseEnter += new EventHandler(control_MouseEnter);
+                        newCtrl.MouseLeave += new EventHandler(control_MouseLeave);
+                        newCtrl.MouseDown += new MouseEventHandler(control_MouseDown);
+                        newCtrl.MouseMove += new MouseEventHandler(control_MouseMove);
+                        newCtrl.MouseUp += new MouseEventHandler(control_MouseUp);
+
+                        this.Controls.Add(newCtrl);
+                        _ctrl = newCtrl;
+                    }
+
+
+
+                    _ctrl.Top = this.PointToClient(new Point(e.X, e.Y)).Y;
+                    _ctrl.Left = this.PointToClient(new Point(e.X, e.Y)).X;
+                }
+            }
+        }
+
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Control ctrl = sender as Control;
+
+            if (ctrl.Capture && e.Button == MouseButtons.Left)
+            {
+                DoDragDrop(ctrl, DragDropEffects.Move);//定義拖曳圖示
+            }
+        }
+
+
+
+        private MouseEventArgs _pos = null;
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                _pos = e;//按下時記錄位置
+            }
+        }
+
+
+
+        private void control_MouseEnter(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            Cursor = Cursors.SizeAll;
+        }
+        private void control_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
+            timer1.Start();
+        }
+        private void control_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+
+                this.Invalidate();  //unselect other control
+                SelectedControl = (Control)sender;
+                Control control = (Control)sender;
+                
+
+                mouseX = -e.X;
+                mouseY = -e.Y;
+                control.Invalidate();
+                DrawControlBorder(sender);
+
+            }
+        }
+        private void control_MouseMove(object sender, MouseEventArgs e)
+        {
+            Control control = (Control)sender;
+            if (e.Button == MouseButtons.Left)
+            {
+                
+                
+                Point nextPosition = new Point();
+                nextPosition = this.PointToClient(MousePosition);
+                nextPosition.Offset(mouseX, mouseY);
+                control.Location = nextPosition;
+                Invalidate();
+            }
+
+        }
+        private void control_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Control control = (Control)sender;
+                Cursor.Clip = System.Drawing.Rectangle.Empty;
+                control.Invalidate();
+                DrawControlBorder(control);
+            }
+        }
+        /// <summary>
+        /// Track the mouse movement and display the correct cursor when it's over
+        /// one of the drag handle of the selected control.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            #region Get the direction and display correct cursor
+
+            if (SelectedControl != null)
+            {
+                Point pos = this.PointToClient(MousePosition);
+                //check if the mouse cursor is within the drag handle
+                if ((pos.X >= SelectedControl.Location.X - DRAG_HANDLE_SIZE &&
+                    pos.X <= SelectedControl.Location.X) &&
+                    (pos.Y >= SelectedControl.Location.Y - DRAG_HANDLE_SIZE &&
+                    pos.Y <= SelectedControl.Location.Y))
+                {
+                    direction = Direction.NW;
+                    Cursor = Cursors.SizeNWSE;
+                }
+                else if ((pos.X >= SelectedControl.Location.X + SelectedControl.Width &&
+                    pos.X <= SelectedControl.Location.X + SelectedControl.Width + DRAG_HANDLE_SIZE &&
+                    pos.Y >= SelectedControl.Location.Y + SelectedControl.Height &&
+                    pos.Y <= SelectedControl.Location.Y + SelectedControl.Height + DRAG_HANDLE_SIZE))
+                {
+                    direction = Direction.SE;
+                    Cursor = Cursors.SizeNWSE;
+                }
+                else if ((pos.X >= SelectedControl.Location.X + SelectedControl.Width / 2 - DRAG_HANDLE_SIZE / 2) &&
+                    pos.X <= SelectedControl.Location.X + SelectedControl.Width / 2 + DRAG_HANDLE_SIZE / 2 &&
+                    pos.Y >= SelectedControl.Location.Y - DRAG_HANDLE_SIZE &&
+                    pos.Y <= SelectedControl.Location.Y)
+                {
+                    direction = Direction.N;
+                    Cursor = Cursors.SizeNS;
+                }
+                else if ((pos.X >= SelectedControl.Location.X + SelectedControl.Width / 2 - DRAG_HANDLE_SIZE / 2) &&
+                    pos.X <= SelectedControl.Location.X + SelectedControl.Width / 2 + DRAG_HANDLE_SIZE / 2 &&
+                    pos.Y >= SelectedControl.Location.Y + SelectedControl.Height &&
+                    pos.Y <= SelectedControl.Location.Y + SelectedControl.Height + DRAG_HANDLE_SIZE)
+                {
+                    direction = Direction.S;
+                    Cursor = Cursors.SizeNS;
+                }
+                else if ((pos.X >= SelectedControl.Location.X - DRAG_HANDLE_SIZE &&
+                    pos.X <= SelectedControl.Location.X &&
+                    pos.Y >= SelectedControl.Location.Y + SelectedControl.Height / 2 - DRAG_HANDLE_SIZE / 2 &&
+                    pos.Y <= SelectedControl.Location.Y + SelectedControl.Height / 2 + DRAG_HANDLE_SIZE / 2))
+                {
+                    direction = Direction.W;
+                    Cursor = Cursors.SizeWE;
+                }
+                else if ((pos.X >= SelectedControl.Location.X + SelectedControl.Width &&
+                    pos.X <= SelectedControl.Location.X + SelectedControl.Width + DRAG_HANDLE_SIZE &&
+                    pos.Y >= SelectedControl.Location.Y + SelectedControl.Height / 2 - DRAG_HANDLE_SIZE / 2 &&
+                    pos.Y <= SelectedControl.Location.Y + SelectedControl.Height / 2 + DRAG_HANDLE_SIZE / 2))
+                {
+                    direction = Direction.E;
+                    Cursor = Cursors.SizeWE;
+                }
+                else if ((pos.X >= SelectedControl.Location.X + SelectedControl.Width &&
+                    pos.X <= SelectedControl.Location.X + SelectedControl.Width + DRAG_HANDLE_SIZE) &&
+                    (pos.Y >= SelectedControl.Location.Y - DRAG_HANDLE_SIZE &&
+                    pos.Y <= SelectedControl.Location.Y))
+                {
+                    direction = Direction.NE;
+                    Cursor = Cursors.SizeNESW;
+                }
+                else if ((pos.X >= SelectedControl.Location.X - DRAG_HANDLE_SIZE &&
+                    pos.X <= SelectedControl.Location.X + DRAG_HANDLE_SIZE) &&
+                    (pos.Y >= SelectedControl.Location.Y + SelectedControl.Height - DRAG_HANDLE_SIZE &&
+                    pos.Y <= SelectedControl.Location.Y + SelectedControl.Height + DRAG_HANDLE_SIZE))
+                {
+                    direction = Direction.SW;
+                    Cursor = Cursors.SizeNESW;
+                }
+                else
+                {
+                    Cursor = Cursors.Default;
+                    direction = Direction.None;
+                }
+            }
+            else
+            {
+                direction = Direction.None;
+                Cursor = Cursors.Default;
+            }
+            #endregion
+        }
+        /// <summary>
+        /// Draw a border and drag handles around the control, on mouse down and up.
+        /// </summary>
+        /// <param name="sender"></param>
+        private void DrawControlBorder(object sender)
+        {
+            Control control = (Control)sender;
+            //define the border to be drawn, it will be offset by DRAG_HANDLE_SIZE / 2
+            //around the control, so when the drag handles are drawn they will be seem
+            //connected in the middle.
+            Rectangle Border = new Rectangle(
+                new Point(control.Location.X - DRAG_HANDLE_SIZE / 2,
+                    control.Location.Y - DRAG_HANDLE_SIZE / 2),
+                new Size(control.Size.Width + DRAG_HANDLE_SIZE,
+                    control.Size.Height + DRAG_HANDLE_SIZE));
+            //define the 8 drag handles, that has the size of DRAG_HANDLE_SIZE
+            Rectangle NW = new Rectangle(
+                new Point(control.Location.X - DRAG_HANDLE_SIZE,
+                    control.Location.Y - DRAG_HANDLE_SIZE),
+                new Size(DRAG_HANDLE_SIZE, DRAG_HANDLE_SIZE));
+            Rectangle N = new Rectangle(
+                new Point(control.Location.X + control.Width / 2 - DRAG_HANDLE_SIZE / 2,
+                    control.Location.Y - DRAG_HANDLE_SIZE),
+                new Size(DRAG_HANDLE_SIZE, DRAG_HANDLE_SIZE));
+            Rectangle NE = new Rectangle(
+                new Point(control.Location.X + control.Width,
+                    control.Location.Y - DRAG_HANDLE_SIZE),
+                new Size(DRAG_HANDLE_SIZE, DRAG_HANDLE_SIZE));
+            Rectangle W = new Rectangle(
+                new Point(control.Location.X - DRAG_HANDLE_SIZE,
+                    control.Location.Y + control.Height / 2 - DRAG_HANDLE_SIZE / 2),
+                new Size(DRAG_HANDLE_SIZE, DRAG_HANDLE_SIZE));
+            Rectangle E = new Rectangle(
+                new Point(control.Location.X + control.Width,
+                    control.Location.Y + control.Height / 2 - DRAG_HANDLE_SIZE / 2),
+                new Size(DRAG_HANDLE_SIZE, DRAG_HANDLE_SIZE));
+            Rectangle SW = new Rectangle(
+                new Point(control.Location.X - DRAG_HANDLE_SIZE,
+                    control.Location.Y + control.Height),
+                new Size(DRAG_HANDLE_SIZE, DRAG_HANDLE_SIZE));
+            Rectangle S = new Rectangle(
+                new Point(control.Location.X + control.Width / 2 - DRAG_HANDLE_SIZE / 2,
+                    control.Location.Y + control.Height),
+                new Size(DRAG_HANDLE_SIZE, DRAG_HANDLE_SIZE));
+            Rectangle SE = new Rectangle(
+                new Point(control.Location.X + control.Width,
+                    control.Location.Y + control.Height),
+                new Size(DRAG_HANDLE_SIZE, DRAG_HANDLE_SIZE));
+
+            //get the form graphic
+            Graphics g = this.CreateGraphics();
+            //draw the border and drag handles
+            ControlPaint.DrawBorder(g, Border, Color.Gray, ButtonBorderStyle.Dotted);
+            ControlPaint.DrawGrabHandle(g, NW, true, true);
+            ControlPaint.DrawGrabHandle(g, N, true, true);
+            ControlPaint.DrawGrabHandle(g, NE, true, true);
+            ControlPaint.DrawGrabHandle(g, W, true, true);
+            ControlPaint.DrawGrabHandle(g, E, true, true);
+            ControlPaint.DrawGrabHandle(g, SW, true, true);
+            ControlPaint.DrawGrabHandle(g, S, true, true);
+            ControlPaint.DrawGrabHandle(g, SE, true, true);
+            g.Dispose();
+            
+        }
+
+
+
+        enum Direction
+        {
+            NW,
+            N,
+            NE,
+            W,
+            E,
+            SW,
+            S,
+            SE,
+            None
+        }
+    }
+}
+
